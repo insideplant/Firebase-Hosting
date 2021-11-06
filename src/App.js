46d4d7d331d7a509d94bccd07324ef69e8fca66b
insideplant <kami.10pearl@gmail.com>
2021-11-07 00:59:16 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Form, InputGroup, Input, Button, Table } from 'reactstrap';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    addTodo(value)
+    setValue('')
+  }
+
+  const addTodo = text => {
+    const newTodos = [...todos, text]
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="mt-4">Todo リスト</h1>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Input type="text"
+              value={value}
+              onChange={e => setValue(e.target.value)} />
+              <Button type="submit" color="primary">追加</Button>
+          </InputGroup>
+        </Form>
+      </Container>
+      <Container>
+        <Table>
+          <tbody>
+            {todos && todos.map((todo, index) => (
+              <tr key={index}>
+                <th className="text-start">
+                  {todo}
+                </th>
+                <td className="text-end" >
+                  <Button color="danger" onClick={() => removeTodo(index)}>削除</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
